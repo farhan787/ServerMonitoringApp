5405c954a32c9e07469b95ed7f7e41b8a604d0ca
Muhammad Farhan <farhanshah.96930012@gmail.com>
2018-12-12 03:56:18 +0530

    tokens handlers added

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ var unifiedServer = function(req, res) {
     var queryString = parsedUrl.query
 
     // get the headers as an object
-    var header = req.headers
+    var headers = req.headers
 
     // get the http method
     var method = req.method.toLowerCase()
@@ -72,7 +72,7 @@ var unifiedServer = function(req, res) {
             trimmedPath,
             queryString,
             method,
-            header,
+            headers,
             'payload': helpers.parseJsonToObject(buffer)
         }
 
@@ -104,5 +104,6 @@ var unifiedServer = function(req, res) {
 // Define a request router
 var router = {
     'ping': handlers.ping,
-    'users': handlers.users
+    'users': handlers.users,
+    'tokens': handlers.tokens
 }
