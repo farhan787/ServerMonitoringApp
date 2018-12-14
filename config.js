ed1b671a8c1b80a34a5854418a5436cea7335696
Muhammad Farhan <farhanshah.96930012@gmail.com>
2018-12-14 19:41:03 +0530

    Checks CRUD handlers for users added

--- config.js
+++ config.js
@@ -5,7 +5,8 @@ environments.staging = {
     'httpPort': 3000,
     'httpsPort': 3001,
     'envName': 'staging',
-    'hashingSecret': 'This is a secret'
+    'hashingSecret': 'This is a secret',
+    'maxChecks': 5
 }
 
 // production default environment
@@ -13,7 +14,8 @@ environments.production = {
     'httpPort': 5000,
     'httpsPort': 5001,
     'envName': 'production',
-    'hashingSecret': 'This is also a secret'
+    'hashingSecret': 'This is also a secret',
+    'maxChecks': 5
 }
 
 // determine the environment passed in command line
